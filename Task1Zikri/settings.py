a5ed208804e8ad8cfa22298b210ce4012b323a7f
XiaoZik <81950435+XiaoZik@users.noreply.github.com>
2022-07-30 23:45:12 +0800

    Update settings.py

--- Task1Zikri/settings.py
+++ Task1Zikri/settings.py
@@ -84,7 +84,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME' : 'NotesDatabase',
         'USER': 'postgres',
-        'PASSWORD': 'jktm3ytf',
+        'PASSWORD': '',
         'HOST': 'localhost',
         'PORT': '',
     }
